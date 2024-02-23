2251da316014f3ecd2598308417429e1a2d1b69f
oksana12345678 <144157970+oksana12345678@users.noreply.github.com>
2024-02-23 18:42:14 +0100

    add

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,11 +1,45 @@
-const form = document.querySelector('.form');
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  const elements = event.currentTarget.elements;
+  const delayInput = this.elements.delay;
+  const delay = parseInt(delayInput.value);
 
-  console.log(elements);
+  const stateInput = this.elements.state;
+  const state = stateInput.value;
+  const promise = new Promise((resolve, reject) => {
+    if (state === 'fulfilled') {
+      setTimeout(() => {
+        resolve(delay);
+      }, delay);
+    } else if (state === 'rejected') {
+      setTimeout(() => {
+        reject(delay);
+      }, delay);
+    }
+  });
+
+  promise.then(
+    delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    },
+
+    delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    }
+  );
+  event.currentTarget.reset();
 }
